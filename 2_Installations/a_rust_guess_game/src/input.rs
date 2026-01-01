bf320130c11b09f4b2a9e434ca0af9335037c0d2
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-02 00:28:51 +0530

    Updates

--- 2_Installations/a_rust_guess_game/src/input.rs
+++ 2_Installations/a_rust_guess_game/src/input.rs
@@ -1,1 +1,18 @@
-pub fn read_new_guess() -> u32 {}
+use std::io;
+
+pub fn read_dificulty() -> String {
+    let mut input: String = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("⛔ Failure to read.");
+    input.trim().to_lowercase()
+}
+
+pub fn read_new_guess() -> u32 {
+    let mut input: String = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("⛔ Failure to read.");
+    let n: u32 = input.trim().parse::<u32>().unwrap_or_default();
+    n
+}
